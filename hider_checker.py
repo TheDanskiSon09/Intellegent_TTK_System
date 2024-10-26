344a34efcf2de9b6b1237adc1a93b7727ccf4d67
Skidan Vladislav <139117484+TheDanskiSon09@users.noreply.github.com>
2024-10-26 18:27:03 +0300

    Delete hider_checker.py

--- hider_checker.py
+++
@@ -1,8 +0,0 @@
-from hammett.conf import settings
-from hammett.core.hiders import HidersChecker
-
-
-class TTKHidersChecker(HidersChecker):
-    async def is_admin(self, update, _context) -> bool:
-        user = update.effective_user
-        return str(user.id) in settings.ADMIN_GROUP
